3ee902069b0c83b59eb74d513d9e4c34d67e736d
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-13 16:16:49 -0500

    turn script from exercise1.27 into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolioCost(filename):
+    totalCost = 0.0  # initiliaze a floating point variable
+    with open(filename, 'rt') as f: # read in csv file
+        headers = next(f)                 # grab the first line of headers
+        for line in f:                    # loop thru lines and use values
+            row = line.split(',')         # populate variables
+            nshares = int(row[1])         # first value is number of shares
+            price = float(row[2])         # 2nd value is price
+            totalCost += nshares * price  # calculate cost of each stock and sum
+    return totalCost
