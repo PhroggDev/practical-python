1ca78931477a320edc4988067849bc1876ba15da
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-13 18:11:34 -0500

    use csv module in portfolioCost function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,18 @@
 # pcost.py
-#
-# Exercise 1.27
+import csv
 def portfolioCost(filename):
+    '''
+    Computes the total cost (shares*price) of a portfolio file
+    '''
     totalCost = 0.0  # initiliaze a floating point variable
-    with open(filename, 'rt') as f: # read in csv file
-        headers = next(f)                 # grab the first line of headers
-        for line in f:                    # loop thru lines and use values
-            row = line.split(',')  # populate variables
+    with open(filename, 'rt') as f:  # read in csv file
+        rows = csv.reader(f)
+        headers = next(rows)                # grab the first line, headers
+        for row in rows:                    # loop thru lines and use values
             try:
-                nshares = int(row[1])  # first value is number of shares
-            except ValueError as e:
-                print("Missing or incorrect data type: " + str(e))
-            try:
-                price = float(row[2])  # 2nd value is price
-            except ValueError:
-                print("Missing or incorrect data type: " + (e))
-            totalCost += nshares * price  # calculate cost of each stock and sum
+                nshares = int(row[1])       # first value is number of shares
+                price = float(row[2])       # 2nd value is price
+                totalCost += nshares * price  # calculate cost of each stock and sum
+            except ValueError:  # catch missing or bad values
+                print("Bad row: " + str(row))
     return totalCost
