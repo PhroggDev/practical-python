670d670e26d80bf31377da2b717eca41be48d508
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-13 15:39:25 -0500

    add comments

--- Work/ex1.27.py
+++ Work/ex1.27.py
@@ -1,11 +1,11 @@
 # pcost.py
-totalCost = 0.0
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        totalCost += nshares * price
+totalCost = 0.0 # initiliaze a floating point variable
+with open('Data/portfolio.csv', 'rt') as f: # read in csv file
+    headers = next(f)                     # grab the first line of headers
+    for line in f:                        # loop thru lines and use values to 
+        row = line.split(',')             # populate variables
+        nshares = int(row[1])             # first value is number of shares
+        price = float(row[2])             # 2nd value is price
+        totalCost += nshares * price      # calculate cost of each stock and sum
 
 print(f"Total cost of portfolio: {totalCost}")
