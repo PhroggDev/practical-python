054936b02446f8183795c0cdb9e1efd8c30fe538
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-12 21:10:08 -0500

    Accessing elements of a string

---
+++ Work/ex1.13.py
@@ -0,0 +1,7 @@
+symbols = 'AAPL,IBM,MSFT,YHOO,SCO'
+# Print some extracted letters
+print(symbols[0])
+print(symbols[1])
+print(symbols[2])
+print(symbols[-1])  # Last character
+print(symbols[-2])  # Negative index counts from end of string
