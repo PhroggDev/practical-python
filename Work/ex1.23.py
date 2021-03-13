423b6cce2f90ca478a2da50df37965d6651da16c
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-13 01:47:14 -0500

    sorting list

---
+++ Work/ex1.23.py
@@ -0,0 +1,7 @@
+symlist = ['HPQ', 'AA', 'AAPL', 'AIG', 'GOOG', 'RHT', 'YHOO']
+# Alphabetic sort is default mode eh
+symlist.sort()
+print(symlist)
+# Now a revers sort
+symlist.sort(reverse=True)
+print(symlist)
