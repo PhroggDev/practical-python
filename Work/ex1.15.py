85df292f29e6a76474b398c64bb8406901e87ee6
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-12 21:48:05 -0500

    testing for substrings within a string

---
+++ Work/ex1.15.py
@@ -0,0 +1,4 @@
+symbols = 'AAPL,IBM,MSFT,YHOO,SCO'
+print(f"'IBM' is in our string") if 'IBM' in symbols else print(f"'IBM' is not in our string")
+print(f"'AA' is in our string") if 'AA' in symbols else print(f"'AA' is not in our string")
+print(f"'CAT' is in our string") if 'CAT' in symbols else print(f"'CAT' is not in our string")
