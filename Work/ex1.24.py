e71192e55018b3f50a1a2903dfaca564b3b6c122
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-13 01:52:07 -0500

    transform a list into a string with join()

---
+++ Work/ex1.24.py
@@ -0,0 +1,8 @@
+symlist = ['HPQ', 'AA', 'AAPL', 'AIG', 'GOOG', 'RHT', 'YHOO']
+# use join to turn a list into a string
+a = '.'.join(symlist)
+print(a)
+b = ' '.join(symlist)
+print(b)
+c = ','.join(symlist)
+print(c)
