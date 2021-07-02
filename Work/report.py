156092ed44de579eb7e1a2d4eb8ca5f121c227e1
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-07-02 06:02:35 -0400

    add report.py, reads csv into list of tuples data structure

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,23 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        try:
+            for row in rows:
+                holding = (row[0],int(row[1]),float(row[2]))
+                portfolio.append(holding)
+        except ValueError:
+            print('Missing or Bad value')
+            next(rows)
+    return portfolio
+    # if len(sys.argv) == 2:
+    #     filename = sys.argv[1]
+    # else:
+    # filename = 'Data/portfolio.csv'
