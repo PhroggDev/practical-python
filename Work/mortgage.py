aea3dba442bd45cc1b41f48436826e9078109393
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-07-23 14:18:17 -0400

    Pretty up with Linter suggestions

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,22 +5,22 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-months = 0
+mos = 0
 
 extra_payment = 1000
-extra_payment_start_month = 61
-extra_payment_end_month = 108
+extra_payment_strt_mo = 61
+extra_payment_end_mo = 108
 
 while principal > 0:
     principal = principal * (1 + rate/12) - payment
     total_paid += payment
-    months += 1
+    mos += 1
 
-    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+    if mos >= extra_payment_strt_mo and mos <= extra_payment_end_mo:
         principal -= extra_payment
         total_paid += extra_payment
-    print("Month: " + str(months) + " TotalPaid: " + str(round(total_paid,2)) +
-          " PrincipalRemaining: " + str(round(principal,2)))
+    print("Month: " + str(mos) + " TotalPaid: " + str(round(total_paid, 2)) +
+          "PrincipalRemaining: " + str(round(principal, 2)))
 
 print("Total paid: ", round(total_paid, 2))
 # print("It took " + str(months) + " months to payoff loan")
