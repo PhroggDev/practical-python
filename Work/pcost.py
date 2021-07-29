f78a6285047f0e9c7513c53a0f4cb7cfe6620abd
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-07-29 09:50:03 -0400

    Use zip to to create dict object from headers and rows

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,26 @@
 # pcost.py
 #
 # Exercise 2.15
+import csv
+
+
 def portfolioCost(filename):
     totalCost = 0
     with open(filename, 'rt') as f:
-        # headers = next(f).split(',')
-        rows = f.read()
-    for rowno, row in enumerate(rows, start=1):
-        try:
-            nshares = int(row[1])
-            price = float(row[2])
-            totalCost += nshares * price
-        except ValueError:
-            print(f'Row {rowno}: Bad row: {row}')
-            next(f)
+        rows = csv.reader(f)
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                totalCost += nshares * price
+                # Catch errors in above int() or float() conversions
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+                next(f)
 
     return totalCost
 
 
-print(f"Total cost: {portfolioCost('Data/portfolio.csv'):.2f}")
+# print(f"Total cost: {portfolioCost('Data/portfolio.csv'):.2f}")
