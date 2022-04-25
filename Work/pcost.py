3a1b5f1549230f4a3c05786b056997bd04decb82
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2022-04-25 17:22:46 -0400

    Exercise 1.32: use Python Library to read a CSV file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,26 +1,12 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.32
 import csv
-import sys
-def portfolioCost(filename):
-    totalCost = 0
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        try:
-            for row in rows:
-                nshares = int(row[1])
-                price = float(row[2])
-                totalCost += nshares * price
-        except ValueError:
-            print('Missing or Bad value')
-            next(rows)
-    return totalCost
-# print(f'Total cost: {portfolioCost('Data/portfolio.csv'):.2f}')
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
-cost = portfolioCost(filename)
-print(f'Total cost: {cost}')
+
+f = open('Data/portfolio.csv')
+rows = csv.reader(f)
+headers = next(rows)
+print(headers)
+for row in rows:
+    print(row)
+f.close()
