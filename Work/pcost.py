76f4cff9635d1828bc305c85379d0297a57f921f
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2022-04-25 17:07:16 -0400

    Exercise 1.30; edit pcost.py to calculate portfolio cost from csv file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,20 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.30
 totalCost = 0
-with open('Data/portfolio.csv','rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        totalCost += nshares * price
-print(f'Total cost: {totalCost:.2f}')
+
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            row = line.split(',')
+            nshares = int(row[1])
+            price = float(row[2])
+            total += nshares * price
+    return total
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost: {cost:0.2f}')
