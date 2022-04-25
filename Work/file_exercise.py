7bc0b5389d507710fe0ee1039fecf7332dbe934e
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2022-04-25 14:51:32 -0400

    read from cities.json sample data, throw error msg on error

---
+++ Work/file_exercise.py
@@ -0,0 +1,23 @@
+# file_exercise.py
+
+import json
+
+
+def main():
+    with open("cities.json") as cities_file:
+        try:
+            cities_data = json.load(cities_file)
+
+            print("Largest cities in the US by population:")
+            for index, entry in enumerate(cities_data):
+                print(f"{index + 1}: {entry['name']} - {entry['pop']}")
+
+        except json.decoder.JSONDecodeError as error:
+            print("Sorry, there was an error decoding that json file:")
+            print(f"\t {error}")
+
+        print("Closing data files...")
+
+
+if __name__ == "__main__":
+    main()
