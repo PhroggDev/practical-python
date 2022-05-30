1409d3710bdc6c59b98f2fcec664cdec9ab650c7
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2022-05-30 12:53:35 -0400

    Start moving Exercises into dirys not just branches

--- Work/1_6/sears.py
+++ Work/1_6/sears.py
@@ -8,9 +8,10 @@ day = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
+    # day = days + 1    # FIX THIS
     day += 1
     num_bills *= 2
 
 print('Number of days ', day)
 print('Number of bills ', num_bills)
-print('Final height: ', num_bills * bill_thickness)
+print(f'Final height {num_bills * bill_thickness} meters')
