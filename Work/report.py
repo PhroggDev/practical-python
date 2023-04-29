54778c2be975f90c3e69d4fb0651223bf2a85cff
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2023-04-29 10:06:52 -0400

    rewrite Exercise 2.4 replacing tuples with dictionaries

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.5
 import csv
 def read_portfolio(filename):
     """Computes the total cost (shares*price) of a portfolio file"""
@@ -9,6 +9,20 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {}
+            holding['name'] = row[0]
+            holding['shares'] = int(row[1])
+            holding['price'] = float(row[2])
             portfolio.append(holding)
     return portfolio
+
+
+def read_prices(filename):
+    """Update data object with 'current' list of prices from file"""
+    prices = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) != 0: prices.append(row)
+            else: continue
+    return prices
