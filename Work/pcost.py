1faf838784161fb3e85daea5c83e2ae9e3b078d5
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2023-05-01 17:21:52 -0400

    change variable names to camel case

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,26 +5,26 @@ import csv
 import sys
 
 
-def portfolioCost(filename):
-    totalCost = 0
-    with open(filename) as f:
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         try:
             for row in rows:
                 nshares = int(row[1])
                 price = float(row[2])
-                totalCost += nshares * price
+                total_cost += nshares * price
         except ValueError:
             print('Missing or Bad value')
             next(rows)
-    return totalCost
-# print(f'Total cost: {portfolioCost('Data/portfolio.csv'):.2f}')
+    return total_cost
 
+# print(f'Total cost: {portfolio_cost('Data/portfolio.csv'):.2f}')
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = 'Data/portfolio.csv'
-cost = portfolioCost(filename)
+cost = portfolio_cost(filename)
 print(f'Total cost: {cost}')
