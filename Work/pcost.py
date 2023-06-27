640f1befbcfcaba306b0a401a432adf5083ce40c
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2023-06-27 00:06:29 -0400

    make pcost.py look more like the course provided code

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,23 +1,21 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 2.4
 import csv
 import sys
 
 
 def portfolio_cost(filename):
+    '''Computes total cost (shares x price) of a portfolio file'''
     total_cost = 0
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        try:
-            for row in rows:
-                nshares = int(row[1])
-                price = float(row[2])
-                total_cost += nshares * price
-        except ValueError:
-            print('Missing or Bad value')
-            next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
     return total_cost
 
 # print(f'Total cost: {portfolio_cost('Data/portfolio.csv'):.2f}')
