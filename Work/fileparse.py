47269f7665871b019b91ca26a85beb8f6ff4e34c
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2023-12-02 11:46:04 -0500

    Exercise 3.4 builds a column selector

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,11 +1,11 @@
 """fileparse.py for Exercise3.1"""
 # fileparse.py
 #
-# Exercise 3.3
+# Exercise 3.4
 import csv
 
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     """
     Parse a CSV file into a list of records
     """
@@ -14,10 +14,24 @@ def parse_csv(filename):
 
         # Read the file headers
         headers = next(rows)
+
+        # if a column selector was given, find indices of the specified columns,
+        # Also narrow the set of headers used for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:  # Skip empty rows
                 continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+
+            # Make a dictionary out of records returned
             record = dict(zip(headers, row))
             records.append(record)
     return records
