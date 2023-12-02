63f3cfcdaf5f7ed5f4cd4895a30de52c6181bce0
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2023-12-02 11:31:27 -0500

    initial version of fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,23 @@
+"""fileparse.py for Exercise3.1"""
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+
+def parse_csv(filename):
+    """
+    Parse a CSV file into a list of records
+    """
+    with open(filename, encoding="utf-8") as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:  # Skip empty rows
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+    return records
