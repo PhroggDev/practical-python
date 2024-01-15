f5c81531eb60744ccf48d5e0e14f3fd500bdad23
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2024-01-15 11:18:23 -0500

    merge changes from refactored pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,23 +6,21 @@ import sys
 
 
 def portfolio_cost(filename):
-    '''Computes total cost (shares x price) of a portfolio file'''
-    total_cost = 0
-
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
+    total = 0
+    with open(filename, "rt") as f:
+        headers = next(f).split(",")
+        for line in f:
+            row = line.split(",")
             nshares = int(row[1])
             price = float(row[2])
-            total_cost += nshares * price
-    return total_cost
+            total += nshares * price
+    return total
 
-# print(f'Total cost: {portfolio_cost('Data/portfolio.csv'):.2f}')
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = 'Data/portfolio.csv'
+    filename = "Data/portfolio.csv"
+
 cost = portfolio_cost(filename)
-print(f'Total cost: {cost}')
+print("Total cost of portfolio:", cost)
