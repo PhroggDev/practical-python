0b263346ed7f45590ec7526374758a33b3c55dd7
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2026-01-11 13:38:24 -0500

    What the hell, push all found changes

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,11 +6,14 @@ import sys
 
 
 def portfolio_cost(filename):
+    """Compute total cost of (shares*price) of a portfolio
+    file"""
     total = 0
+
     with open(filename, "rt") as f:
-        headers = next(f).split(",")
-        for line in f:
-            row = line.split(",")
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             nshares = int(row[1])
             price = float(row[2])
             total += nshares * price
